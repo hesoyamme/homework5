e6b6767806bd472856a730e56ff15d35f8558c69
hesoyamme <borisov.tt2@gmail.com>
2025-05-10 16:08:42 +0300

    Task 3 complete

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,19 @@ public class Main {
 //            }
 //            System.out.println();
 //        }
+///  Task 3
+        int[][] arr = new int [5][5];
+        int count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i >= j){
+                    arr[i][j] = count;
+                }
+                if (arr[i][j] > 0) {
+                    System.out.print(arr[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
