9ca6d071344458b34fd55873a5f7e8ca109fcd4c
hesoyamme <borisov.tt2@gmail.com>
2025-05-10 16:00:05 +0300

    Task 2 complete

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    int[][] arr = new int[10][10];
-    int count = 1;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (j < 100){
-                    arr[i][j] = count++;
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(arr));
+/// Task 1
+//    int[][] arr = new int[10][10];
+//    int count = 1;
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                if (j < 100){
+//                    arr[i][j] = count++;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(arr));
+    /// Task 2
+//        int[][] arr = new int [5][5];
+//        int count = 1;
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                if (i >= j){
+//                    arr[i][j] = count;
+//                }
+//                System.out.print(arr[i][j]);
+//            }
+//            System.out.println();
+//        }
     }
 }
