b3b10e805f4d37f1122a3dbdeaf29777ab7553bf
hesoyamme <borisov.tt2@gmail.com>
2025-05-14 19:34:48 +0300

    Task 3 complete

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,7 @@ public class Main {
     int count = 1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-//                if (j < 100){
                     arr[i][j] = count++;
-//                }
             }
         }
         System.out.println(Arrays.deepToString(arr));
