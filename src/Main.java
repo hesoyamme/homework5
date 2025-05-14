9d334564453899b1fe9bca94b98e842a7e26c0e8
hesoyamme <borisov.tt2@gmail.com>
2025-05-14 19:34:26 +0300

    Task 3 complete

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 /// Task 1
-//    int[][] arr = new int[10][10];
-//    int count = 1;
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr[i].length; j++) {
+    int[][] arr = new int[10][10];
+    int count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
 //                if (j < 100){
-//                    arr[i][j] = count++;
+                    arr[i][j] = count++;
 //                }
-//            }
-//        }
-//        System.out.println(Arrays.deepToString(arr));
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
     /// Task 2
 //        int[][] arr = new int [5][5];
 //        int count = 1;
@@ -26,18 +26,18 @@ public class Main {
 //            System.out.println();
 //        }
 ///  Task 3
-        int[][] arr = new int [5][5];
-        int count = 1;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i >= j){
-                    arr[i][j] = count;
-                }
-                if (arr[i][j] > 0) {
-                    System.out.print(arr[i][j]);
-                }
-            }
-            System.out.println();
-        }
+//        int[][] arr = new int [5][5];
+//        int count = 1;
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                if (i >= j){
+//                    arr[i][j] = count;
+//                }
+//                if (arr[i][j] > 0) {
+//                    System.out.print(arr[i][j]);
+//                }
+//            }
+//            System.out.println();
+//        }
     }
 }
